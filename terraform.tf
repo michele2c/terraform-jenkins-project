4a536c969e93f966069930429d332daf7a4ca1af
Michele Costa Christianson <michpimentel22@gmail.com>
2024-02-18 17:11:36 -0700

    provider update

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.36.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
